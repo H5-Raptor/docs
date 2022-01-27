5bd9c58285d195faa5fdbbfb38b2a93bb9b6fedd
jobyrao <jobyrao@tencent.com>
2022-01-27 22:40:17 +0800

    feat: 新增部分文档

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -76,6 +76,7 @@ module.exports = {
         collapsable: false, // 可选的, 默认值是 true,
         children: [
           "/advanced/code",
+          "/advanced/utils-plugin",
         ]
       },
       {
